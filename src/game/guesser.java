13bb0e7bd33941c2884dce331b857280313c74c4
Ricky Gupta <rg4825@rit.edu>
2021-12-21 23:34:25 -0500

    Added implementation to file guesser.java.

--- src/game/guesser.java
+++ src/game/guesser.java
@@ -1,8 +1,29 @@
 package game;
 
+import java.util.ArrayList;
+
 /**
  * Provides the state of the guesser role.
  */
 public class guesser {
+    private ArrayList<Character> guessedLetters;
+    private int numGuessed;
 
+    public guesser() {
+        this.guessedLetters = new ArrayList<>();
+        this.numGuessed = 0;
+    }
+
+    public void guessLetter(char letter) {
+        this.guessedLetters.add(letter);
+        this.numGuessed++;
+    }
+
+    public ArrayList<Character> getGuessedLetters() {
+        return this.guessedLetters;
+    }
+
+    public int getNumGuessed() {
+        return this.numGuessed;
+    }
 }
