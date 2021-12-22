68d6586eac51237ea50b1fea2d0f433ecd0673f5
Ricky Gupta <rg4825@rit.edu>
2021-12-21 23:28:18 -0500

    Added file words_alpha.txt. Added implementation to executioner.java.

--- src/game/executioner.java
+++ src/game/executioner.java
@@ -4,5 +4,13 @@ package game;
  * Provides the state of the executioner role.
  */
 public class executioner {
+    private final String guessWord;
 
+    public executioner(String word) {
+        guessWord = word;
+    }
+
+    public String getGuessWord() {
+        return this.guessWord;
+    }
 }
